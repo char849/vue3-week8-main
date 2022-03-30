72cb769a027710e04239015ddfb96c3c9b56a5aa
charlotte <charlottelee849@gmail.com>
2022-03-30 21:41:24 +0800

    git init

--- src/components/CheckOutStep.vue
+++ src/components/CheckOutStep.vue
@@ -2,7 +2,7 @@
   <ul class="row justify-content-center list-unstyled py-3 px-3">
     <li class="col-md-4">
       <div
-        :class="[step === 'cart' ? 'text-primary' : 'text-muted']"
+        :class="[step === 'cart' ? 'text-primary,bg-secondary' : 'text-muted']"
         class="bg-primary p-3 badge rounded-pill fs-3 w-100 mb-2 bg-light"
       >
         1. 填寫訂單
